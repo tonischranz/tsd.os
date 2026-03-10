e3788c386de2e17d3db926d5be654e1edfbfb750
Toni Schranz <toni.schranz@gmail.com>
2026-03-10 18:36:52 +0100

    Update tsd.bash

--- tsd.bash
+++ tsd.bash
@@ -185,7 +185,7 @@ alias l='lynx'
 alias c='curl'
 alias v='vim -c \"vs.|vertical resize 32|wincmd w|bel term\"'
 alias g='grep'
-alias f='find . -type f -print0 | xargs -0 grep --color=auto'
+alias f='find . -type f -print0 | xargs -0 grep'
 ## todo FreeBSD find
 alias n='nano'
 alias t='top'
